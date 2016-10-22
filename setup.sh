fed184f0d369b880feeea0d408bfd00ae9455934
Axel Pardemann <axelitusdev@gmail.com>
2016-10-21 19:23:58 -0500

    Added a feature to install a framework automatically through composer

--- setup.sh
+++ setup.sh
@@ -2,6 +2,39 @@
 # Install required roles from roles.yml file
 ansible-galaxy install --roles-path ./.ansible/roles -r ./.ansible/roles.yml
 
+# If an argument was given install the requested framework through composer
+frmwrk=$1
+version=$2
+cmd=""
+
+if [ "$frmwrk" != "" ]; then
+	if [ "$frmwrk" == "laravel" ]; then
+		cmd="composer create-project --prefer-dist laravel/laravel"
+	elif [ "$frmwrk" == "fuel" ]; then
+		cmd="composer create-project --prefer-dist fuel/fuel"
+	fi
+
+	# If we found a supported framework then finish the command and execute it
+	if [ "$cmd" != "" ]; then
+		# If we have a specific version append it to the command
+		if [ "$version" != "" ]; then
+			cmd=$cmd":${version}"
+		fi
+		# Set the command destination folder
+		cmd=$cmd" src"
+
+		# To install a framework we need to have an empty src folder
+		rm -rf src/public
+
+		# Execute framework command
+		#eval $cmd
+		echo "Running: $cmd"
+		eval $cmd
+	else
+		echo "Unsupported framework, no changes made..."
+	fi
+fi
+
 # Remove this setup script
 echo "Removing setup script..."
 rm ./setup.sh
